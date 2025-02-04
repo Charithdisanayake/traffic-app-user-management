c4b5c3d7880737de38ca6a63c06702a8c92e6776
charith <charithrasika26@gmail.com>
2025-02-04 15:12:57 +0530

    fix build issues

--- src/main/java/com/traffic/usermng/UserMngApplication.java
+++ src/main/java/com/traffic/usermng/UserMngApplication.java
@@ -1,4 +1,4 @@
-package com.trafffic.User_mng;
+package com.traffic.usermng;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/traffic/usermng/UserMngApplicationTests.java
+++ src/test/java/com/traffic/usermng/UserMngApplicationTests.java
@@ -1,4 +1,4 @@
-package com.trafffic.User_mng;
+package com.traffic.usermng;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
